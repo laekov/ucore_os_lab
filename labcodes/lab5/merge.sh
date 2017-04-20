960ea77bb145d6bee9bf39d3ee3145b82c656a8e
laekov <laekov.h@gmail.com>
2017-04-20 11:12:28 +0800

    lab5

---
+++ labcodes/lab5/merge.sh
@@ -0,0 +1,15 @@
+x=kern/mm/pmm.c
+vimdiff $x ../lab4/$x
+exit
+x=kern/mm/default_pmm.c
+vimdiff $x ../lab4/$x
+x=kern/mm/pmm.c
+vimdiff $x ../lab4/$x
+x=kern/mm/swap_fifo.c
+vimdiff $x ../lab4/$x
+x=kern/mm/vmm.c
+vimdiff $x ../lab4/$x
+x=kern/process/proc.c
+vimdiff $x ../lab4/$x
+x=kern/trap/trap.c
+vimdiff $x ../lab4/$x
